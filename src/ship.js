fc017122fcca77ce7d6ab5358d7cdaaa1d813036
gonzalo_fuentes <fubagg@kyndryl.com>
2024-12-09 09:36:40 -0300

    feat: add tests for ship class

---
+++ src/ship.js
@@ -0,0 +1,19 @@
+class Ship {
+    constructor(length) {
+        this._length = length;
+        this._hits = 0;
+    }
+
+    getLength() {
+        return this._length;
+    }
+    
+    isSunk() {
+        return this._hits >= this._length;
+    }
+
+    hit() {
+        this._hits++;
+    }
+}
+export {Ship}
---
+++ src/tests/ship.test.js
@@ -0,0 +1,30 @@
+import {Ship} from "../ship"
+
+describe('Ship class', () => {
+    it('creates a ship element with the given length', () =>  {
+        const newShip = new Ship(5);
+        expect(newShip).toBeInstanceOf(Ship)
+        expect(newShip.getLength()).toEqual(5);
+    });
+
+    it('registers hits correctly', () => {
+        const newShip = new Ship(5);
+        newShip.hit();
+        newShip.hit();
+        expect(newShip._hits).toEqual(2);
+    });
+
+    it('is not sunk if the number of hits is lower than length', () => {
+        const newShip = new Ship(5);
+        newShip.hit();
+        newShip.hit();
+        expect(newShip.isSunk()).toBe(false);
+    }); 
+
+    it('is sunk if the number of hits is equal to length', () => {
+        const newShip = new Ship(2);
+        newShip.hit();
+        newShip.hit();
+        expect(newShip.isSunk()).toBe(true);
+    }); 
+}) 
