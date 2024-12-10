be2533085973786db67d791757f60911fd9573ac
gekishou <gonfuentes112@gmail.com>
2024-12-10 17:27:19 -0300

    feat: add player class

---
+++ src/player.js
@@ -0,0 +1,11 @@
+import { Gameboard } from "./gameboard";
+
+class Player {
+    constructor() {
+        this._board = new Gameboard();
+    }
+
+    get board() {
+        return this._board;
+    }
+}
