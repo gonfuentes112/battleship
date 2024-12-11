c34dfc43fb38008b3f78c79a8d1ff139cbe7b025
gekishou <gonfuentes112@gmail.com>
2024-12-11 13:12:18 -0300

    feat: add ships object

--- src/gamelogic.js
+++ src/gamelogic.js
@@ -1,1 +1,17 @@
 import { Gameboard } from "./gameboard";
+import { Player } from "./player";
+
+function gameLogic() {
+    const human = new Player("p1");
+    const cpu = new Player("p2");
+
+    const ships = {
+        "carrier" : {length : 5},
+        "battleship" : {length : 4},
+        "destroyer" : {length : 3},
+        "submarine" : {length : 3},
+        "patrolboat" : {length : 2},
+    }
+
+    
+}
