a46c807356c083eaee0db7c1db7933c62cdf76ad
gekishou <gonfuentes112@gmail.com>
2024-12-13 19:16:51 -0300

    refactor: move board initialization to uimodules

--- src/gameui.js
+++ src/gameui.js
@@ -1,30 +1,10 @@
 import { Gameboard } from "./gameboard";
 import { gameLogic } from "./gamelogic";
+import {initializeCpuBoard, initializePlayerBoard} from "./uimodules"
 
 function gameUi() {
-    const cpuboard = document.getElementById('cpuboard');
-    for (let row = 0; row < 10; row++){
-        for (let column = 0; column < 10; column++){
-            const cell = document.createElement('div');
-            cell.classList.add('cell');
-            cell.classList.add('cpublank');
-            cell.dataset.row = row;
-            cell.dataset.column = column;
-            cpuboard.appendChild(cell)
-        }
-    }
-    const playerboard = document.getElementById('playerboard');
-    for (let row = 0; row < 10; row++){
-        for (let column = 0; column < 10; column++){
-            const cell = document.createElement('div');
-            cell.classList.add('cell');
-            cell.classList.add('playerblank');
-            cell.dataset.row = row;
-            cell.dataset.column = column;
-            playerboard.appendChild(cell)
-        }
-    }
-
+    initializeCpuBoard();
+    initializePlayerBoard();
     const startButton = document.getElementById('start');
     const resetButton = document.getElementById('reset');
     startButton.classList.toggle('hidden');
